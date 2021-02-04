81e96e9df7146f6d212f54999362a54e1847e33e
hoang2692 <hoang2692@gmail.com>
2021-02-04 14:42:25 +0700

    update

--- Api/Controllers/notification/notification.js
+++ Api/Controllers/notification/notification.js
@@ -1,9 +1,21 @@
 var admin = require ('firebase-admin');
 var serviceAccount = require ('./key.json');
+var Token = require('../../../modals/notification')
 admin.initializeApp ({
     credential: admin.credential.cert (serviceAccount),
   });
 let Notification = {
+  getToken: async (req,res) =>{
+    const findToken = await Token.findOne({token: req.body.token})
+    if(!findToken){
+      const newDevice = await Token.create(req.body);
+      res.json(newDevice)
+    }
+    else
+    {
+      res.status(200).send({messenger: 'Device is valid'})
+    }
+  },
   sendToAll: async (req, res) => {
     var registrationToken = 'cBeG1B0qRoyYcheX1DBwGd:APA91bGNFw764S4m_QRS19PkFH-le9A-4UyCf02924ZpkYGqprtVVhc9uxRiAHQJ3DhB_C4ndd_8wX4mkFGsHVeUiLSzojEGOCYmwQCFpOBM9xkYT08xFMHSRaRQcpLtUkY5lbKFqcHb';
     var message = {
--- Api/Routers/notification.js
+++ Api/Routers/notification.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express();
 const notificationControllers = require('../Controllers/notification/notification');
 router.post('/sendAll',notificationControllers.sendToAll)
-
+router.post('/recived/token',notificationControllers.getToken)
 module.exports = router;
