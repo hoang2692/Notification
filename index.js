74691ce206d32bc5f2db653c32fccfa4ed6a6ded
hoang2692 <hoang2692@gmail.com>
2021-02-20 01:05:02 +0700

    update

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
-const app = express();
-var server = require("http").Server(app);
-var io = require("socket.io")(server)
-const port = process.env.PORT || 5000;
-const CustomerRoute = require('./Api/Routers/customerRoute')
-require('./middleware/socket')(io)
+
 mongoose.connect(
     'mongodb+srv://admin:admin@cluster0.jdpe7.mongodb.net/ChatsApp?retryWrites=true&w=majority',{
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true })
         .then(db => console.log("Connect Success"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use('/',CustomerRoute)
-server.listen(port, () =>console.log("Hello " + port))
+const newapp = express();
+const port =process.env.PORT || 5000;
+var CustomerRoute = require('./Api/Routers/customerRoute')
+newapp.use(bodyParser.json());
+newapp.use(bodyParser.urlencoded({ extended: true }));
+newapp.use(express.static('public'));
+newapp.use('/', CustomerRoute)
+newapp.listen(port, () =>console.log("Hello " + port))
